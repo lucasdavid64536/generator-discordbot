a3aa01758b61c7699d594d65b74b9a6bb87e836e
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-12-17 17:28:13 +0200

    Update config.js

--- generators/app/templates/config.js
+++ generators/app/templates/config.js
@@ -2,17 +2,17 @@ module.exports = {
 
   // Your bot name. Typically, this is your bot's username without the discriminator.
   // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-  name: "<%= botName %>",
+  name: "NoryChat",
 
   // The bot's command prefix. The bot will recognize as command any message that begins with it.
   // i.e: "<%= prefix %> foo" will trigger the command "foo",
   //      whereas "<%= botName %> foo" will do nothing at all.
-  prefix:  "<%= prefix %>",
+  prefix:  "o!",
 
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: "<%= token %>",
+  token: "TOKEN",
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
